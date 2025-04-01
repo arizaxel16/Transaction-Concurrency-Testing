79b1e444fec6650c13d665f45fded42e855eddc9
arizaxel16 <arizaxel16@gmail.com>
2025-04-01 15:08:09 -0500

    Add repositories

---
+++ transaction-app/src/main/java/com/transactionapp/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.transactionapp.repository;
+
+import com.transactionapp.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
